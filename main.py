e5f3d84d29fb2ca36471e60781f621193fc4c68a
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-06-29 10:49:25 +0530

    Finish basic networking, Create bugs for future self, Update UI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO, join_room, leave_room
+from flask import Flask, render_template, request, redirect, url_for, session  
+from flask_socketio import SocketIO, join_room, leave_room, send
 from random import choices 
 from string import ascii_uppercase
 
@@ -18,15 +18,73 @@ def GenerateCode(length):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    session.clear()
+
     if request.method=="POST":
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
+        if join:
+            if not code:
+                return render_template("index.html", error="Please enter a code!")
+        
+        room = code 
+
+        if create:
+            room = GenerateCode(5)
+            rooms[room] = {"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "members": 0}
+        elif code not in rooms:
+            return render_template("index.html", error="Invalid code!")
+        
+        session["room"] = room
+        
+        return redirect(url_for("home"))
     return render_template("index.html")
 
-@app.route("/game")
+@app.route("/game/")
 def home():
-    return render_template("board.html")
+    room = session.get("room")
+    if room is None or room not in rooms:
+        return redirect(url_for("index"))
+    return render_template("board.html", room=room, fen=rooms[room]["fen"])
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    if not room:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return 
+    if rooms[room]["members"]>=2: # Only 2 players allowed
+        leave_room(room)
+        return
+    join_room(room)
+    send({"tag": "announcement", "message": "has joined!"}, to=room)
+    rooms[room]["members"] += 1
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"]<=0:
+            del rooms[room]  
+    send({"tag": "announcement", "message": "has left!"}, to=room)
+
+@socketio.on("move")
+def move(data):
+    room = session.get("room")
+    name = session.get("name")
+    if room not in rooms:
+        return 
+    content = {
+        "tag": "move",
+        "move": data["move"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append({"name": name, "message": data["data"]})
 
 if __name__ == "__main__":
     socketio.run(app)
--- static/board-script.js
+++ static/board-script.js
@@ -1,3 +1,5 @@
+var socketio = io();
+
 var game = new Chess()
 
 function onDragStart (source, piece, position, orientation) {
@@ -9,55 +11,26 @@ function onDragStart (source, piece, position, orientation) {
         (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
       return false
     }
-  }
-  
-  function onDrop (source, target) {
-    // see if the move is legal
-    var move = game.move({
-      from: source,
-      to: target,
-      promotion: 'q' // NOTE: always promote to a queen for example simplicity
-    })
-  
-    // illegal move
-    if (move === null) return 'snapback'
-  
-    updateStatus()
-  }
-  
-  // update the board position after the piece snap
-  // for castling, en passant, pawn promotion
-  function onSnapEnd () {
-    board.position(game.fen())
-  }
-  
-  function updateStatus () {
-    var status = ''
-  
-    var moveColor = 'White'
-    if (game.turn() === 'b') {
-      moveColor = 'Black'
-    }
-  
-    // checkmate?
-    if (game.in_checkmate()) {
-      status = 'Game over, ' + moveColor + ' is in checkmate.'
-    }
-  
-    // draw?
-    else if (game.in_draw()) {
-      status = 'Game over, drawn position'
-    }
-  
-    // game still on
-    else {
-      status = moveColor + ' to move'
-  
-      // check?
-      if (game.in_check()) {
-        status += ', ' + moveColor + ' is in check'
-      }
-    }
+}
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  SendFen(game.fen());
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
 }
 
 var config = {
@@ -70,3 +43,22 @@ var config = {
 }
 
 var board = Chessboard('board', config)
+
+const content_messages = document.querySelector(".messages");
+
+const ChangeFen = (fen) => {
+    game.load(fen);
+    board.position(fen);
+}
+
+socketio.on("message", (data) => {
+    if (data.tag=="announcement") {
+        console.log(data.message);
+        return;
+    }
+    ChangeFen(data.fen);
+});
+
+const SendFen = (fen) => {
+    socketio.emit("move", {tag: "move", move: fen});
+};
